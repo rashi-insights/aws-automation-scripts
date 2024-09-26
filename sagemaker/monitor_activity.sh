6505ff38f1c68989cb82c46450057e330711d2bd
Soham Gadhave <soham.gadhave@somaiya.edu>
2024-09-26 18:28:22 +0530

    print CPU Usage in percent every 10 secs

--- sagemaker/monitor_activity.sh
+++ sagemaker/monitor_activity.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
 # Do not run this script as a standalone script. If you do just make sure to add these variables:
-# REGION
-# PROFILE
-# APP_TYPE
-# APP_NAME
-# DOMAIN_ID
-# SPACE_NAME
 # AWS_ACCESS_KEY_ID
 # AWS_SESSION_TOKEN
 # AWS_SECRET_ACCESS_KEY
 # This script is meant to run somewhere where these variables are injected into the environment.
 
 # Configuration
+IDLE_COUNTER=0             # Idle counter
+CHECK_INTERVAL=10          # Check interval in seconds
 IDLE_CPU_THRESHOLD=15      # CPU percentage threshold
 IDLE_TIME_THRESHOLD=120    # Idle time in seconds
-CHECK_INTERVAL=10          # Check interval in seconds
-IDLE_COUNTER=0             # Idle counter
 
-# aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile $PROFILE
-# aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile $PROFILE
-# aws configure set aws_session_token $AWS_SESSION_TOKEN --profile $PROFILE
+# AWS Options
+REGION="us-east-2"
+APP_NAME="default"
+APP_TYPE="JupyterLab"
+DOMAIN_ID="d-cofiway7apio"
+PROFILE="lifecycle-config-script"
+SPACE_NAME="private-automation-test"
+
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile $PROFILE
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile $PROFILE
+aws configure set aws_session_token $AWS_SESSION_TOKEN --profile $PROFILE
 
 # Function to check CPU usage
 check_cpu_usage() {
